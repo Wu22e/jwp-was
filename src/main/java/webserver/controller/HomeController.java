ba7551f6a37ff26f5fa34ddbc485499cbc0d0b71
wu2ee <gulb1602@gmail.com>
2022-08-18 23:20:53 +0900

    refactor : 메서드 시작 bracket 띄어쓰기 추가

--- src/main/java/webserver/controller/HomeController.java
+++ src/main/java/webserver/controller/HomeController.java
@@ -4,7 +4,7 @@ import webserver.http.request.HttpRequest;
 import webserver.http.request.requestline.Path;
 import webserver.http.response.HttpResponse;
 
-public class HomeController extends MethodController{
+public class HomeController extends MethodController {
     @Override
     HttpResponse processGet(HttpRequest httpRequest) {
         return HttpResponse.redirect("/index.html");
